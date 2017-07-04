d7f0af5c3085daa6c5807c3307274583e61687c7
bearbear12345 <andrew.j.wong@outlook.com>
2017-07-04 22:28:35 +1000

    Delete unnecessary files in the build directory

--- program/helper/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0/../source
-rmdir /q /s build
-"C:\Program Files (x86)\Python\3.6.0\python.exe" setup.py build
--- program/source/setup.py
+++ program/source/setup.py
@@ -5,7 +5,7 @@ import sys, OnCue
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = ["OnCue"], include_files = ["OnCue.ico", "D:\Program Files (x86)\VideoLAN\VLC\plugins", "D:\Program Files (x86)\VideoLAN\VLC\libvlc.dll", "D:\Program Files (x86)\VideoLAN\VLC\libvlccore.dll"], excludes = [], optimize = 2, silent = False, build_exe = "../build/")
+buildOptions = dict(packages = ["oncue"], include_files = ["OnCue.ico", "D:\Program Files (x86)\VideoLAN\VLC\plugins", "D:\Program Files (x86)\VideoLAN\VLC\libvlc.dll", "D:\Program Files (x86)\VideoLAN\VLC\libvlccore.dll"], excludes = ["urllib", "logging", "imageformats", "unittest", "pydoc_data", "email", "html", "xml", "http"], optimize = 2, silent = False, build_exe = "../build/")
 
 base = 'Win32GUI' if sys.platform == 'win32' else None
 
